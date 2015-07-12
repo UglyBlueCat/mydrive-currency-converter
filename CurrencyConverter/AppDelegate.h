7fc3f3424f9deb3cb9b607f127ccacc9b166bbac
Robin Spinks <uglybluecat@googlemail.com>
2015-07-12 12:36:38 +0100

    Initial Commit

---
+++ CurrencyConverter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CurrencyConverter
+//
+//  Created by Robin Spinks on 12/07/2015.
+//  Copyright (c) 2015 Robin Spinks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
